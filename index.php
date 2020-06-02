d3bf125fcb45b37d3b683c722fd8068d70ce7122
volandr <34952271+volandr@users.noreply.github.com>
2020-06-02 14:25:29 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="stylesheet" href="styles/bootstrap.min.css">
+	<link rel="stylesheet" href="styles/main.css">
+	<title>Документ</title>
+</head>
+<body>
+	<h1>Автотехцентр «‎Pnevmo-Auto»‎</h1>
+	<div class="akcia container">
+		<div class="row">
+			<div class="col-lg-9">
+				<img src="images/fire.svg" alt="">
+				<h3 class="align-middle">СНИЖЕНИЕ ЦЕН -30% <small>на запчасти для пневмоподвески</small></h3>
+			</div>
+			<div class="col-lg-3" >
+				<div class="dropdown">
+				 <button class="btn btn-light dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+				    <span>УЗНАТЬ ЦЕНЫ</span>
+				  </button>
+				  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+				  	<a href="" class="dropdown-item">AUDI</a>
+				  	<a href="" class="dropdown-item">BENTLEY</a>
+				  	<a href="" class="dropdown-item">BMW</a>
+				  	<a href="" class="dropdown-item">DODGE</a>
+				  	<a href="" class="dropdown-item">HUMMER</a>
+				  	<a href="" class="dropdown-item">HYUNDAY</a>
+				  	<a href="" class="dropdown-item">JEEP</a>
+				  	<a href="" class="dropdown-item">KIA</a>
+				  	<a href="" class="dropdown-item">LAND ROVER</a>
+				  	<a href="" class="dropdown-item">MERSEDES BENZ</a>
+				  	<a href="" class="dropdown-item">PORSCHE</a>
+				  	<a href="" class="dropdown-item">TOYOTA</a>
+				  	<a href="" class="dropdown-item">VOLKSWAGEN</a>
+				  	<a href="" class="dropdown-item">VOLVO</a>
+				  </div>
+			</div
+	
+		</div>
+	</div>
+</div>
+</div>
+<div class="container uslugi">
+	<div class="row">
+		<div class="col-sm col-md">
+			  <div class="card-body">
+			     <img src="images/usl1.jpg" class="card-img-top" alt="...">
+					<button type="button" class="btn btn-dark btn-block">РЕМОНТ ПНЕВМОПОДВЕСКИ</button>
+			  </div>
+		</div>
+		<div class="col-sm col-md">
+			  <div class="card-body">
+			     <img src="images/usl2.jpg" class="card-img-top" alt="...">
+					<button type="button" class="btn btn-dark btn-block">РЕМОНТ РУЛЕВЫХ РЕЕК</button>
+			  </div>
+		</div>
+		<div class="ccol-sm col-md">
+			  <div class="card-body">
+			     <img src="images/usl3.jpg" class="card-img-top" alt="...">
+					<button type="button" class="btn btn-dark btn-block">3D РАЗВАЛ-СХОЖДЕНИЕ</button>
+			  </div>
+		</div>
+	</div>
+</div>
+
+
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+</body>
+</html>
